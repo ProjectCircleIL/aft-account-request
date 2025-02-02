477f2fe52f20e57eb69ddc1906b21339402a2b14
Omri Siri <omri@projectcircle.co.il>
2025-02-02 19:48:25 +0200

    Add another account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "develop-02" {
 
   account_customizations_name = "develop-02"
 }
+
+module "develop-03" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws+dev03@projectcircle.co.il"
+    AccountName               = "develop-03"
+    ManagedOrganizationalUnit = "Development"
+    SSOUserEmail              = "aws+dev03@projectcircle.co.il"
+    SSOUserFirstName          = "develop-03"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "AFT" = "develop-03"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AFT Controller"
+    change_reason       = "Vending Development Account 03"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "develop-03"
+}
